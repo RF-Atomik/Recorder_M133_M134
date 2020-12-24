035c7a5b81e61ea8bc37425687fcdca082bad857
RF-Atomik <alexis.felin@calipro.fr>
2020-12-24 13:57:23 +0100

    update

--- MQT_MAT_M134.sh
+++ MQT_MAT_M134.sh
@@ -1,2 +1,2 @@
 gnome-terminal --working-directory="/home/odroidh2/Documents/Recorder_M134" -- /bin/sh -c 'sleep 10 ;./upload; exec bash'
-gnome-terminal --working-directory="/home/odroidh2/Documents/Recorder_M134" -- /bin/sh -c 'sleep 30;./recorder -c 1536p -a -cr 2 -cd X264 -l 3600 -i -fps 5; exec bash'/
+gnome-terminal --working-directory="/home/odroidh2/Documents/Recorder_M134" -- /bin/sh -c 'sleep 30;./recorder -c 1536p -a -cr 2 -cd X264 -l 3600 -i -fps 5 -d 0; exec bash'/
